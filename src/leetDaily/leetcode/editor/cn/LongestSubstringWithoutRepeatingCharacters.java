530f193697d30b8bd888d6a540ca9300b8928e48
zcyzhuangzhou <2738524026@qq.com>
2021-03-03 22:12:45 +0800

    寻找两个正序数组的中位数

--- src/leetDaily/leetcode/editor/cn/LongestSubstringWithoutRepeatingCharacters.java
+++ src/leetDaily/leetcode/editor/cn/LongestSubstringWithoutRepeatingCharacters.java
@@ -46,6 +46,9 @@
 // 👍 5041 👎 0
 
 package leetDaily.leetcode.editor.cn;
+
+import java.util.*;
+
 public class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
         Solution solution = new LongestSubstringWithoutRepeatingCharacters().new Solution();
@@ -53,9 +56,137 @@ public class LongestSubstringWithoutRepeatingCharacters {
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-
+        if (s.equals("")) {
+            return 0;
+        }
+        ArrayList<Integer> length = new ArrayList<Integer>();
+        char[] s_arr = s.toCharArray();
+        for(int i = 0; i < s_arr.length; i++) {
+            Map<Character, Integer> hashtable = new HashMap<Character, Integer>();
+            for (int j = i; j < s_arr.length; j ++) {
+                if (hashtable.containsKey(s_arr[j])) {
+                    length.add(hashtable.size());
+                    i = hashtable.get(s_arr[j]);
+                    break;
+                }
+                hashtable.put(s_arr[j], j);
+                if (j == s_arr.length - 1) {
+                    length.add(hashtable.size());
+                }
+            }
+        }
+        return Collections.max(length);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
+
+
+
+/*
+
+
+1 / 9
+
+代码：
+
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        //s[start,end) 前面包含 后面不包含
+        int start(0), end(0), length(0), result(0);
+        int sSize = int(s.size());
+        while (end < sSize)
+        {
+            char tmpChar = s[end];
+            for (int index = start; index < end; index++)
+            {
+                if (tmpChar == s[index])
+                {
+                    start = index + 1;
+                    length = end - start;
+                    break;
+                }
+            }
+
+            end++;
+            length++;
+            result = max(result, length);
+        }
+        return result;
+    }
+};
+
+1 / 8
+
+代码：
+
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        //s[start,end) 前面包含 后面不包含
+        int start(0), end(0), length(0), result(0);
+        int sSize = int(s.size());
+        unordered_map<char, int> hash;
+        while (end < sSize)
+        {
+            char tmpChar = s[end];
+            //仅当s[start,end) 中存在s[end]时更新start
+            if (hash.find(tmpChar) != hash.end() && hash[tmpChar] >= start)
+            {
+                start = hash[tmpChar] + 1;
+                length = end - start;
+            }
+            hash[tmpChar] = end;
+
+            end++;
+            length++;
+            result = max(result, length);
+        }
+        return result;
+    }
+};
+
+1 / 9
+
+代码：
+
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        //s[start,end) 前面包含 后面不包含
+        int start(0), end(0), length(0), result(0);
+        int sSize = int(s.size());
+        vector<int> vec(128, -1);
+        while (end < sSize)
+        {
+            char tmpChar = s[end];
+            //仅当s[start,end) 中存在s[end]时更新start
+            if (vec[int(tmpChar)] >= start)
+            {
+                start = vec[int(tmpChar)] + 1;
+                length = end - start;
+            }
+            vec[int(tmpChar)] = end;
+
+            end++;
+            length++;
+            result = max(result, length);
+        }
+        return result;
+    }
+};
+
+
+作者：pinku-2
+链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/solution/wu-zhong-fu-zi-fu-de-zui-chang-zi-chuan-cshi-xian-/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
