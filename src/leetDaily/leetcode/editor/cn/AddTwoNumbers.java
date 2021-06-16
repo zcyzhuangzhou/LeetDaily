b606c298aa6addc848d8adcbe7eef8a9cf20dfe6
zcyzhuangzhou <2738524026@qq.com>
2021-06-16 15:14:33 +0800

    refactor

--- src/leetDaily/leetcode/editor/cn/AddTwoNumbers.java
+++ src/leetDaily/leetcode/editor/cn/AddTwoNumbers.java
@@ -1,181 +1,181 @@
-
-//给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
 //
-// 请你将两个数相加，并以相同形式返回一个表示和的链表。 
+////给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+////
+//// 请你将两个数相加，并以相同形式返回一个表示和的链表。
+////
+//// 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+////
+////
+////
+//// 示例 1：
+////
+////
+////输入：l1 = [2,4,3], l2 = [5,6,4]
+////输出：[7,0,8]
+////解释：342 + 465 = 807.
+////
+////
+//// 示例 2：
+////
+////
+////输入：l1 = [0], l2 = [0]
+////输出：[0]
+////
+////
+//// 示例 3：
+////
+////
+////输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+////输出：[8,9,9,9,0,0,0,1]
+////
+////
+////
+////
+//// 提示：
+////
+////
+//// 每个链表中的节点数在范围 [1, 100] 内
+//// 0 <= Node.val <= 9
+//// 题目数据保证列表表示的数字不含前导零
+////
+//// Related Topics 递归 链表 数学
+//// 👍 5707 👎 0
 //
-// 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。 
+//package leetcode.editor.cn;
+//public class AddTwoNumbers {
+//    public static void main(String[] args) {
+//        Solution solution = new Solution();
+////        ListNode l3 = new ListNode();
+////        l3 = solution.addTwoNumbers(new ListNode(6, new ListNode(1, new ListNode(0))), new ListNode(7, new ListNode(4, new ListNode(2, new ListNode(1, new ListNode(1))))));
+//    }
+//    //leetcode submit region begin(Prohibit modification and deletion)
+///**
+// * Definition for singly-linked list.
+// * public class ListNode {
+// *     int val;
+// *     ListNode next;
+// *     ListNode() {}
+// *     ListNode(int val) { this.val = val; }
+// *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+// * }
+// */
+////class ListNode {
+////    int val;
+////    ListNode next;
+////    ListNode() {}
+////    ListNode(int val) { this.val = val; }
+////    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+////}
 //
-// 
-//
-// 示例 1： 
-//
-// 
-//输入：l1 = [2,4,3], l2 = [5,6,4]
-//输出：[7,0,8]
-//解释：342 + 465 = 807.
-// 
-//
-// 示例 2： 
-//
-// 
-//输入：l1 = [0], l2 = [0]
-//输出：[0]
-// 
-//
-// 示例 3： 
-//
-// 
-//输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
-//输出：[8,9,9,9,0,0,0,1]
-// 
-//
-// 
-//
-// 提示： 
-//
-// 
-// 每个链表中的节点数在范围 [1, 100] 内 
-// 0 <= Node.val <= 9 
-// 题目数据保证列表表示的数字不含前导零 
-// 
-// Related Topics 递归 链表 数学 
-// 👍 5707 👎 0
-
-package leetDaily.leetcode.editor.cn;
-public class AddTwoNumbers {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
+//class Solution {
+//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 //        ListNode l3 = new ListNode();
-//        l3 = solution.addTwoNumbers(new ListNode(6, new ListNode(1, new ListNode(0))), new ListNode(7, new ListNode(4, new ListNode(2, new ListNode(1, new ListNode(1))))));
-    }
-    //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-//class ListNode {
-//    int val;
-//    ListNode next;
-//    ListNode() {}
-//    ListNode(int val) { this.val = val; }
-//    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//        ListNode tail = new ListNode();
+//        int flag = 0;
+//        int tmp = 0;
+//        int l1_val, l2_val, l3_val;
+//        while(l1 != null || l2 != null) {
+//            l1_val = l1 != null ? l1.val : 0;
+//            l2_val = l2 != null ? l2.val : 0;
+//            if(l1_val + l2_val + tmp > 9) {
+//                l3_val = l1_val + l2_val + tmp - 10;
+//                tmp = 1;
+//            }
+//            else {
+//                l3_val = l1_val + l2_val + tmp;
+//                tmp = 0;
+//            }
+//            if(flag == 0) {
+//                l3.val = l3_val;
+//                tail = l3;
+//                flag = 1;
+//            }
+//            else {
+//                tail.next = new ListNode(l3_val);
+//                tail = tail.next;
+//            }
+//
+//
+//            if(l1 != null) {
+//                l1 = l1.next;
+//            }
+//            if(l2 != null) {
+//                l2 = l2.next;
+//            }
+//        }
+//        if(tmp == 1) {
+//            tail.next = new ListNode(tmp);
+//            return l3;
+//        }
+//        else {
+//            return l3;
+//        }
+//
+//
+//    }
 //}
-
-public class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode l3 = new ListNode();
-        ListNode tail = new ListNode();
-        int flag = 0;
-        int tmp = 0;
-        int l1_val, l2_val, l3_val;
-        while(l1 != null || l2 != null) {
-            l1_val = l1 != null ? l1.val : 0;
-            l2_val = l2 != null ? l2.val : 0;
-            if(l1_val + l2_val + tmp > 9) {
-                l3_val = l1_val + l2_val + tmp - 10;
-                tmp = 1;
-            }
-            else {
-                l3_val = l1_val + l2_val + tmp;
-                tmp = 0;
-            }
-            if(flag == 0) {
-                l3.val = l3_val;
-                tail = l3;
-                flag = 1;
-            }
-            else {
-                tail.next = new ListNode(l3_val);
-                tail = tail.next;
-            }
-
-
-            if(l1 != null) {
-                l1 = l1.next;
-            }
-            if(l2 != null) {
-                l2 = l2.next;
-            }
-        }
-        if(tmp == 1) {
-            tail.next = new ListNode(tmp);
-            return l3;
-        }
-        else {
-            return l3;
-        }
-
-
-    }
-}
-//leetcode submit region end(Prohibit modification and deletion)
-
-}
-
-
-
-/*
-📖 文字题解
-方法一：模拟
-思路与算法
-
-由于输入的两个链表都是逆序存储数字的位数的，因此两个链表中同一位置的数字可以直接相加。
-
-我们同时遍历两个链表，逐位计算它们的和，并与当前位置的进位值相加。具体而言，如果当前两个链表处相应位置的数字为 n1,n2n1,n2，进位值为 \textit{carry}carry，则它们的和为 n1+n2+\textit{carry}n1+n2+carry；其中，答案链表处相应位置的数字为 (n1+n2+\textit{carry}) \% 10(n1+n2+carry)%10，而新的进位值为 \lfloor\frac{n1+n2+\textit{carry}}{10}\rfloor⌊
-10
-n1+n2+carry
-​
- ⌋。
-
-如果两个链表的长度不同，则可以认为长度短的链表的后面有若干个 00 。
-
-此外，如果链表遍历结束后，有 \textit{carry} > 0carry>0，还需要在答案链表的后面附加一个节点，节点的值为 \textit{carry}carry。
-
-代码
-
-C++JavaJavaScriptGolangC
-
-class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = null, tail = null;
-        int carry = 0;
-        while (l1 != null || l2 != null) {
-            int n1 = l1 != null ? l1.val : 0;
-            int n2 = l2 != null ? l2.val : 0;
-            int sum = n1 + n2 + carry;
-            if (head == null) {
-                head = tail = new ListNode(sum % 10);
-            } else {
-                tail.next = new ListNode(sum % 10);
-                tail = tail.next;
-            }
-            carry = sum / 10;
-            if (l1 != null) {
-                l1 = l1.next;
-            }
-            if (l2 != null) {
-                l2 = l2.next;
-            }
-        }
-        if (carry > 0) {
-            tail.next = new ListNode(carry);
-        }
-        return head;
-    }
-}
-复杂度分析
-
-时间复杂度：O(\max(m,n))O(max(m,n))，其中 m,nm,n 为两个链表的长度。我们要遍历两个链表的全部位置，而处理每个位置只需要 O(1)O(1) 的时间。
-空间复杂度：O(\max(m,n))O(max(m,n))。答案链表的长度最多为较长链表的长度 +1+1。
-
-作者：LeetCode-Solution
-链接：https://leetcode-cn.com/problems/add-two-numbers/solution/liang-shu-xiang-jia-by-leetcode-solution/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
- */
+////leetcode submit region end(Prohibit modification and deletion)
+//
+//}
+//
+//
+//
+///*
+//📖 文字题解
+//方法一：模拟
+//思路与算法
+//
+//由于输入的两个链表都是逆序存储数字的位数的，因此两个链表中同一位置的数字可以直接相加。
+//
+//我们同时遍历两个链表，逐位计算它们的和，并与当前位置的进位值相加。具体而言，如果当前两个链表处相应位置的数字为 n1,n2n1,n2，进位值为 \textit{carry}carry，则它们的和为 n1+n2+\textit{carry}n1+n2+carry；其中，答案链表处相应位置的数字为 (n1+n2+\textit{carry}) \% 10(n1+n2+carry)%10，而新的进位值为 \lfloor\frac{n1+n2+\textit{carry}}{10}\rfloor⌊
+//10
+//n1+n2+carry
+//​
+// ⌋。
+//
+//如果两个链表的长度不同，则可以认为长度短的链表的后面有若干个 00 。
+//
+//此外，如果链表遍历结束后，有 \textit{carry} > 0carry>0，还需要在答案链表的后面附加一个节点，节点的值为 \textit{carry}carry。
+//
+//代码
+//
+//C++JavaJavaScriptGolangC
+//
+//class Solution {
+//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//        ListNode head = null, tail = null;
+//        int carry = 0;
+//        while (l1 != null || l2 != null) {
+//            int n1 = l1 != null ? l1.val : 0;
+//            int n2 = l2 != null ? l2.val : 0;
+//            int sum = n1 + n2 + carry;
+//            if (head == null) {
+//                head = tail = new ListNode(sum % 10);
+//            } else {
+//                tail.next = new ListNode(sum % 10);
+//                tail = tail.next;
+//            }
+//            carry = sum / 10;
+//            if (l1 != null) {
+//                l1 = l1.next;
+//            }
+//            if (l2 != null) {
+//                l2 = l2.next;
+//            }
+//        }
+//        if (carry > 0) {
+//            tail.next = new ListNode(carry);
+//        }
+//        return head;
+//    }
+//}
+//复杂度分析
+//
+//时间复杂度：O(\max(m,n))O(max(m,n))，其中 m,nm,n 为两个链表的长度。我们要遍历两个链表的全部位置，而处理每个位置只需要 O(1)O(1) 的时间。
+//空间复杂度：O(\max(m,n))O(max(m,n))。答案链表的长度最多为较长链表的长度 +1+1。
+//
+//作者：LeetCode-Solution
+//链接：https://leetcode-cn.com/problems/add-two-numbers/solution/liang-shu-xiang-jia-by-leetcode-solution/
+//来源：力扣（LeetCode）
+//著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+// */
--- src/leetDaily/leetcode/editor/cn/MinStack.java
+++ src/leetDaily/leetcode/editor/cn/MinStack.java
@@ -1,178 +1,178 @@
-//设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。 
+////设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+////
+////
+//// push(x) —— 将元素 x 推入栈中。
+//// pop() —— 删除栈顶的元素。
+//// top() —— 获取栈顶元素。
+//// getMin() —— 检索栈中的最小元素。
+////
+////
+////
+////
+//// 示例:
+////
+//// 输入：
+////["MinStack","push","push","push","getMin","pop","top","getMin"]
+////[[],[-2],[0],[-3],[],[],[],[]]
+////
+////输出：
+////[null,null,null,null,-3,null,0,-2]
+////
+////解释：
+////MinStack minStack = new MinStack();
+////minStack.push(-2);
+////minStack.push(0);
+////minStack.push(-3);
+////minStack.getMin();   --> 返回 -3.
+////minStack.pop();
+////minStack.top();      --> 返回 0.
+////minStack.getMin();   --> 返回 -2.
+////
+////
+////
+////
+//// 提示：
+////
+////
+//// pop、top 和 getMin 操作总是在 非空栈 上调用。
+////
+//// Related Topics 栈 设计
+//// 👍 838 👎 0
 //
-// 
-// push(x) —— 将元素 x 推入栈中。 
-// pop() —— 删除栈顶的元素。 
-// top() —— 获取栈顶元素。 
-// getMin() —— 检索栈中的最小元素。 
-// 
+//package leetDaily.leetcode.editor.cn;
 //
-// 
+//import java.util.ArrayList;
+//import java.util.Collections;
 //
-// 示例: 
-//
-// 输入：
-//["MinStack","push","push","push","getMin","pop","top","getMin"]
-//[[],[-2],[0],[-3],[],[],[],[]]
-//
-//输出：
-//[null,null,null,null,-3,null,0,-2]
-//
-//解释：
-//MinStack minStack = new MinStack();
-//minStack.push(-2);
-//minStack.push(0);
-//minStack.push(-3);
-//minStack.getMin();   --> 返回 -3.
-//minStack.pop();
-//minStack.top();      --> 返回 0.
-//minStack.getMin();   --> 返回 -2.
-// 
-//
-// 
-//
-// 提示： 
-//
-// 
-// pop、top 和 getMin 操作总是在 非空栈 上调用。 
-// 
-// Related Topics 栈 设计 
-// 👍 838 👎 0
-
-package leetDaily.leetcode.editor.cn;
-
-import java.util.ArrayList;
-import java.util.Collections;
-
-public class MinStack {
-    public static void main(String[] args) {
-        Solution solution = new MinStack().new Solution();
-    }
-    //leetcode submit region begin(Prohibit modification and deletion)
-class MinStack {
-    ArrayList<Integer> min_stack = new ArrayList<>();
-    int top = -1;
-    /** initialize your data structure here. */
-    public MinStack() {
-
-    }
-    
-    public void push(int x) {
-        min_stack.add(x);
-        top++;
-    }
-    
-    public void pop() {
-        min_stack.remove(top);
-        top--;
-    }
-    
-    public int top() {
-        return min_stack.get(top);
-    }
-    
-    public int getMin() {
-        return Collections.min(min_stack);
-    }
-}
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(x);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
-//leetcode submit region end(Prohibit modification and deletion)
-
-}
-
-
-
-// 使用辅助栈
+//public class MinStack {
+//    public static void main(String[] args) {
+//        Solution solution = new MinStack().new Solution();
+//    }
+//    //leetcode submit region begin(Prohibit modification and deletion)
 //class MinStack {
-//    Deque<Integer> xStack;
-//    Deque<Integer> minStack;
-//
+//    ArrayList<Integer> min_stack = new ArrayList<>();
+//    int top = -1;
+//    /** initialize your data structure here. */
 //    public MinStack() {
-//        xStack = new LinkedList<Integer>();
-//        minStack = new LinkedList<Integer>();
-//        minStack.push(Integer.MAX_VALUE);
+//
 //    }
 //
 //    public void push(int x) {
-//        xStack.push(x);
-//        minStack.push(Math.min(minStack.peek(), x));
+//        min_stack.add(x);
+//        top++;
 //    }
 //
 //    public void pop() {
-//        xStack.pop();
-//        minStack.pop();
+//        min_stack.remove(top);
+//        top--;
 //    }
 //
 //    public int top() {
-//        return xStack.peek();
+//        return min_stack.get(top);
 //    }
 //
 //    public int getMin() {
-//        return minStack.peek();
+//        return Collections.min(min_stack);
 //    }
 //}
-
-
-
-// 不使用额外空间，栈中存储和min的差值diff，注意int范围，需要使用Long存diff
+//
 ///**
-// * stack用来存储和min的差值，min存储最小值，每次出栈的时候通过差值和当前min计算要出栈的值和之前的min
-// * 如果差值diff大于等于0，说明要出栈的值大于等于当前min，那么要出栈的值在入栈的时候没有更新min，返回min+diff；
-// * 如果插值diff小于0，说明当前要出栈的值就是min(因为入栈的时候我们选择的就是min和入栈元素的最小值)，同时，通过min-diff计算出之前min
-// * 要注意的是diff可能会超出int范围，类似于 Integer.MAX_VALUE - 1 这种，所以diff要用Long存
+// * Your MinStack object will be instantiated and called as such:
+// * MinStack obj = new MinStack();
+// * obj.push(x);
+// * obj.pop();
+// * int param_3 = obj.top();
+// * int param_4 = obj.getMin();
 // */
-//class MinStack {
+////leetcode submit region end(Prohibit modification and deletion)
 //
-//    Integer min = null;
-//    Stack<Long> data = new Stack<>();
-//
-//    /**
-//     * initialize your data structure here.
-//     */
-//    public MinStack1() {
-//
-//    }
-//
-//    public void push(int x) {
-//        if (data.isEmpty()) {
-//            data.push(0L);
-//            min = x;
-//        } else {
-//            //如果x是最小的数，这里可能越界，所以用Long来保存
-//            data.push(Long.valueOf(x) - min);
-//            min = Math.min(x, min);
-//        }
-//    }
-//
-//    public void pop() {
-//        Long diff = data.pop();
-//        if (diff >= 0) {
-//            //return min + diff;
-//        } else {
-//            int res = min;
-//            min = (int) (min - diff);
-//            //return res;
-//        }
-//    }
-//
-//    public int top() {
-//        Long diff = data.peek();
-//        if (diff >= 0) {
-//            return (int) (min + diff);
-//        } else {
-//            return min;
-//        }
-//    }
-//
-//    public int getMin() {
-//        return min;
-//    }
 //}
+//
+//
+//
+//// 使用辅助栈
+////class MinStack {
+////    Deque<Integer> xStack;
+////    Deque<Integer> minStack;
+////
+////    public MinStack() {
+////        xStack = new LinkedList<Integer>();
+////        minStack = new LinkedList<Integer>();
+////        minStack.push(Integer.MAX_VALUE);
+////    }
+////
+////    public void push(int x) {
+////        xStack.push(x);
+////        minStack.push(Math.min(minStack.peek(), x));
+////    }
+////
+////    public void pop() {
+////        xStack.pop();
+////        minStack.pop();
+////    }
+////
+////    public int top() {
+////        return xStack.peek();
+////    }
+////
+////    public int getMin() {
+////        return minStack.peek();
+////    }
+////}
+//
+//
+//
+//// 不使用额外空间，栈中存储和min的差值diff，注意int范围，需要使用Long存diff
+/////**
+//// * stack用来存储和min的差值，min存储最小值，每次出栈的时候通过差值和当前min计算要出栈的值和之前的min
+//// * 如果差值diff大于等于0，说明要出栈的值大于等于当前min，那么要出栈的值在入栈的时候没有更新min，返回min+diff；
+//// * 如果插值diff小于0，说明当前要出栈的值就是min(因为入栈的时候我们选择的就是min和入栈元素的最小值)，同时，通过min-diff计算出之前min
+//// * 要注意的是diff可能会超出int范围，类似于 Integer.MAX_VALUE - 1 这种，所以diff要用Long存
+//// */
+////class MinStack {
+////
+////    Integer min = null;
+////    Stack<Long> data = new Stack<>();
+////
+////    /**
+////     * initialize your data structure here.
+////     */
+////    public MinStack1() {
+////
+////    }
+////
+////    public void push(int x) {
+////        if (data.isEmpty()) {
+////            data.push(0L);
+////            min = x;
+////        } else {
+////            //如果x是最小的数，这里可能越界，所以用Long来保存
+////            data.push(Long.valueOf(x) - min);
+////            min = Math.min(x, min);
+////        }
+////    }
+////
+////    public void pop() {
+////        Long diff = data.pop();
+////        if (diff >= 0) {
+////            //return min + diff;
+////        } else {
+////            int res = min;
+////            min = (int) (min - diff);
+////            //return res;
+////        }
+////    }
+////
+////    public int top() {
+////        Long diff = data.peek();
+////        if (diff >= 0) {
+////            return (int) (min + diff);
+////        } else {
+////            return min;
+////        }
+////    }
+////
+////    public int getMin() {
+////        return min;
+////    }
+////}
