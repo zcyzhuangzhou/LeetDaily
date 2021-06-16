1ad752960a82d12c645d728c1cfab5b0fea9b2f8
zcyzhuangzhou <2738524026@qq.com>
2021-06-16 15:28:40 +0800

    [线性查找] 二维数组中的查找

--- src/leetDaily/leetcode/editor/cn/ErWeiShuZuZhongDeChaZhaoLcof.java
+++ src/leetDaily/leetcode/editor/cn/ErWeiShuZuZhongDeChaZhaoLcof.java
@@ -89,3 +89,26 @@ class Solution {
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
+
+/*
+class Solution {
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        int row = 0, column = columns - 1;
+        while (row < rows && column >= 0) {
+            int num = matrix[row][column];
+            if (num == target) {
+                return true;
+            } else if (num > target) {
+                column--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+}
+ */
