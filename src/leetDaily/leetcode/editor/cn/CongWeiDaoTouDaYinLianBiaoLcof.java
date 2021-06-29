6e95011c37fb88b2e946f1efdf886c8ff0209bbe
zcyzhuangzhou <2738524026@qq.com>
2021-06-29 10:44:30 +0800

    [栈 链表] 从尾到头打印链表

---
+++ src/leetDaily/leetcode/editor/cn/CongWeiDaoTouDaYinLianBiaoLcof.java
@@ -0,0 +1,81 @@
+//输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：head = [1,3,2]
+//输出：[2,3,1] 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 链表长度 <= 10000 
+// Related Topics 栈 递归 链表 双指针 
+// 👍 159 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class CongWeiDaoTouDaYinLianBiaoLcof {
+    public static void main(String[] args) {
+        Solution solution = new CongWeiDaoTouDaYinLianBiaoLcof().new Solution();
+        ListNode head = new ListNode(1);
+        ListNode sed = new ListNode(3);
+        ListNode thd = new ListNode(2);
+        head.next = sed;
+        sed.next = thd;
+        System.out.println(Arrays.toString(solution.reversePrint(head)));
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public static class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+class Solution {
+    public int[] reversePrint(ListNode head) {
+        Stack<Integer> stack = new Stack<>();
+        while (head != null) {
+            stack.push(head.val);
+            head = head.next;
+        }
+        int[] arr = new int[stack.size()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = stack.pop();
+        }
+        return arr;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+/* 递归算法
+class Solution {
+    ArrayList<Integer> tmp = new ArrayList<Integer>();
+    public int[] reversePrint(ListNode head) {
+        recur(head);
+        int[] res = new int[tmp.size()];
+        for(int i = 0; i < res.length; i++)
+            res[i] = tmp.get(i);
+        return res;
+    }
+    void recur(ListNode head) {
+        if(head == null) return;
+        recur(head.next);
+        tmp.add(head.val);
+    }
+}
+ */
