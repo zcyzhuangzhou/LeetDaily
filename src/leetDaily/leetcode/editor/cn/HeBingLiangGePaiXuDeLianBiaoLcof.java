b7dcd65c1c2b5a0dae7d86eac02d16b326862c12
zcyzhuangzhou <2738524026@qq.com>
2021-06-30 15:46:25 +0800

    [递归 迭代 链表] 合并两个排序的链表

---
+++ src/leetDaily/leetcode/editor/cn/HeBingLiangGePaiXuDeLianBiaoLcof.java
@@ -0,0 +1,75 @@
+//输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。 
+//
+// 示例1： 
+//
+// 输入：1->2->4, 1->3->4
+//输出：1->1->2->3->4->4 
+//
+// 限制： 
+//
+// 0 <= 链表长度 <= 1000 
+//
+// 注意：本题与主站 21 题相同：https://leetcode-cn.com/problems/merge-two-sorted-lists/ 
+// Related Topics 递归 链表 
+// 👍 132 👎 0
+
+package leetcode.editor.cn;
+public class HeBingLiangGePaiXuDeLianBiaoLcof {
+    public static void main(String[] args) {
+        Solution solution = new HeBingLiangGePaiXuDeLianBiaoLcof().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public static class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val <= l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+/* 迭代方法
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                cur.next = l1;
+                l1 = l1.next;
+            }else{
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 != null ? l1 : l2;
+        return dummy.next;
+    }
+}
+ */
